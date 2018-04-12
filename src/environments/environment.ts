3640afb757e77ca784fb2292149db4b0d0130d90
Aakash Kashyap <aakashkashyap1996@gmail.com>
2018-04-12 23:12:53 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false, 
   firebase: {
-    apiKey: "AIzaSyA3ieDn-zmrvkvAdVNh-LFNCoQGbIS-ZHY",
-    authDomain: "fir-demo-ak07.firebaseapp.com",
-    databaseURL: "https://fir-demo-ak07.firebaseio.com",
-    projectId: "fir-demo-ak07",
-    storageBucket: "fir-demo-ak07.appspot.com",
-    messagingSenderId: "214625693360"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket:"",
+    messagingSenderId: ""
   }
 };
